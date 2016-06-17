462461324e8bf9fca701adb40e43bad83bf6bf8d
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2016-06-18 07:41:50 +0800

    Completed activities model

--- activities/models.py
+++ activities/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 #User Profile Model
@@ -11,17 +12,20 @@ class UserProfile(models.Model):
 #Subject Model
 class Subject(models.Model):
 	name = models.CharField(max_length=200)
-    section = models.ForeignKey(Section, default=1)
-    subject_type = models.ForeignKey(SubjectType, default=0)
-    user = models.ForeignKey(User, default=0)
+    section = models.CharField(max_length=10)
+    teacher = models.ForeignKey(User, default=0)
     school_year = models.CharField(max_length=100, default='2014-2015')
 
     def __unicode__(self):
-        return u'%s %s %s %s %s %s' % (self.id, self.name, self.section.name, self.school_year,
-                                       self.subject_type.name, self.user.get_full_name())
+        return u'%s %s %s %s %s' % (self.id, self.name, self.section, self.school_year, self.teacher.get_full_name())
 
 #Activity Model
+@python_2_unicode_compatible
 class Activity(models.Model):
 	activity_name = models.CharField(max_length=200)
 	activity_description = models.CharField(max_length=200)
-	
+	subject = models.ForeignKey(Subject)
+	deadline = models.DateTimeField()
+
+	def __str__(self):
+		return self.activity_name
