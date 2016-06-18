037656b5de4c8e4dcdf79d5dc058b168d6501ce2
Dynse Clyde Sacote <beetlemon.cwts@gmail.com>
2016-06-18 09:51:14 +0800

    Edited admin.py

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile, Subject, Activity
+
+admin.site.register(Subject)
+admin.site.register(UserProfile)
+admin.site.register(Activity)
